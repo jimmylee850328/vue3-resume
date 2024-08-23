3c1b5af7d25ee0437b8402071dad65ad9b6d1a85
jimmylee850328 <jimmylee850328@gmail.com>
2024-08-24 00:33:35 +0800

    feat: 完成首頁

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,89 +1,93 @@
 <template>
-  <div>123</div>
+  <div class="flex">
+    <!-- 左側邊欄 -->
+    <nav class="w-1/5 bg-[#f5f1e8] h-screen fixed left-0 top-0 p-8 flex flex-col justify-between">
+      <div>
+        <div class="text-[#d1a39e] font-semibold mb-8">
+          <span class="border border-[#d1a39e] px-2 py-1">LUBE CHAO</span>
+        </div>
+        <ul class="space-y-4 text-[#d1a39e]">
+          <li><a href="#page1">首頁</a></li>
+          <li><a href="#page2">關於我</a></li>
+          <li><a href="#page3">經歷</a></li>
+          <li><a href="#page4">作品</a></li>
+          <li><a href="#page5">聯絡我</a></li>
+        </ul>
+      </div>
+      <div class="text-sm text-gray-500">© 2022 LUBE</div>
+    </nav>
+
+    <!-- Fullpage 內容 -->
+    <div class="w-4/5 ml-[20%]">
+      <full-page :options="options" id="fullpage" ref="fullpage">
+        <!-- 第一個 Section -->
+        <div class="section">
+          <div class="flex items-center justify-center h-screen">
+            <div class="relative">
+              <!-- 粉紅色背景框 -->
+              <div class="absolute -top-5 left-5 w-[480px] h-[300px] bg-[#d3756e]"></div>
+
+              <!-- 圖片 -->
+              <img
+                src="https://lube4100.github.io/img/kv-pic.jpg"
+                alt="Beach scene"
+                class="relative z-10 w-[450px] h-[330px] object-cover"
+              />
+
+              <!-- 文字內容 -->
+              <div class="absolute bottom-0 left-0 transform translate-y-full pl-4 pt-2">
+                <p class="text-[#d1a39e] text-lg">TZU-JIE CHAO</p>
+                <div class="w-12 h-0.5 bg-[#d1a39e] mt-2"></div>
+              </div>
+
+              <!-- class="absolute top-0 left-0 transform -rotate-90 origin-bottom-right text-[#d1a39e] text-lg tracking-wider z-10" -->
+              <!-- 垂直文字 -->
+              <div
+                class="absolute top-[50%] left-0 transform -rotate-90 origin-bottom-left text-[#CC6E62] text-lg z-10"
+              >
+                resume
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- 其他 Sections -->
+        <div class="section">
+          <div class="slide">
+            <h3 class="text-center text-2xl">Slide 2.1</h3>
+          </div>
+          <div class="slide">
+            <h3 class="text-center text-2xl">Slide 2.2</h3>
+          </div>
+          <div class="slide">
+            <h3 class="text-center text-2xl">Slide 2.3</h3>
+          </div>
+        </div>
+        <div class="section">
+          <h3 class="text-center text-2xl">Section 3</h3>
+        </div>
+      </full-page>
+    </div>
+  </div>
 </template>
 
-<script>
-// Optional. When using fullpage extensions
-// import './fullpage.scrollHorizontally.min'
+<script setup>
+import { ref } from 'vue'
 
-export default {
-  data() {
-    return {}
-  },
-  methods: {
-    afterLoad() {
-      console.log('After load')
-    },
+const fullpage = ref(null)
 
-    addSection(e) {
-      e.preventDefault()
-      var newSectionNumber = document.querySelectorAll('.fp-section').length + 1
+const options = {
+  licenseKey: 'YOUR_KEY_HERE',
+  afterLoad: afterLoad,
+  scrollOverflow: true,
+  scrollBar: false,
+  menu: '#menu',
+  navigation: true,
+  anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
+  sectionsColor: ['#dcccb2', '#ff5f45', '#0798ec', '#fec401', '#1bcee6']
+}
 
-      // creating the section div
-      var section = document.createElement('div')
-      section.className = 'section'
-      section.innerHTML = `<h3>Section ${newSectionNumber}</h3>`
-
-      // adding section
-      document.querySelector('#fullpage').appendChild(section)
-
-      // creating the section menu element
-      var sectionMenuItem = document.createElement('li')
-      sectionMenuItem.setAttribute('data-menuanchor', 'page' + newSectionNumber)
-      sectionMenuItem.innerHTML = `<a href="#page${newSectionNumber}">Section${newSectionNumber}</a>`
-
-      // adding it to the sections menu
-      var sectionsMenuItems = document.querySelector('#menu')
-      sectionsMenuItems.appendChild(sectionMenuItem)
-
-      // adding anchor for the section
-      this.options.anchors.push(`page${newSectionNumber}`)
-
-      // we have to call `update` manually as DOM changes won't fire updates
-      // requires the use of the attribute ref="fullpage" on the
-      // component element, in this case, <full-page>
-      // ideally, use an ID element for that element too
-      this.$refs.fullpage.build()
-    },
-
-    removeSection() {
-      var sections = document.querySelector('#fullpage').querySelectorAll('.fp-section')
-      var lastSection = sections[sections.length - 1]
-
-      // removing the last section
-      lastSection.parentNode.removeChild(lastSection)
-
-      // removing the last anchor
-      this.options.anchors.pop()
-
-      // removing the last item on the sections menu
-      var sectionsMenuItems = document.querySelectorAll('#menu li')
-      var lastItem = sectionsMenuItems[sectionsMenuItems.length - 1]
-      lastItem.parentNode.removeChild(lastItem)
-    },
-
-    toggleNavigation() {
-      this.options.navigation = !this.options.navigation
-    },
-
-    toggleScrollbar() {
-      console.log('Changing scrollbar...')
-      this.options.scrollBar = !this.options.scrollBar
-    }
-  }
+function afterLoad() {
+  console.log('After load')
 }
 </script>
-
-<style>
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
